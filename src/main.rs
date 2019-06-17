4724ad84b4628d2f576f4b777c55d2fc6d63e425
scottpenn <scottpennd@gmail.com>
2019-06-17 12:21:26 -0700

    Begin work to construct pages lazily, utilizing a common nav and head files.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@
 #[macro_use] extern crate rocket;
 extern crate rocket_contrib;
 
-use std::io::Read;
+#[macro_use]
+extern crate lazy_static;
+
+use std::fs::File;
+use std::fs::read_to_string;
+use std::io;
 use std::io::Error;
 use std::path::{PathBuf, Path};
 use rocket::response::NamedFile;
@@ -10,9 +15,33 @@ use rocket::response::status::NotFound;
 use rocket::response::content::Html;
 use rocket_contrib::serve::StaticFiles;
 
+lazy_static! {
+    static ref HEAD: String = {
+        read_to_string("public/pages/head.html").expect("No file named head.html")
+    };
+    static ref NAV: String = {
+        read_to_string("public/pages/nav.html").expect("No file named nav.html")
+    };
+
+}
+
+fn construct_page(filename: &str) -> String {
+    let mut index = read_to_string(filename).expect(&format!("No file named {}.", filename));
+    index = index.replace("!head!", &*HEAD);
+    index = index.replace("!nav!", &*NAV);
+    index
+}
+
 #[get("/")]
-fn index() -> Result<NamedFile, NotFound<String>> {
-    NamedFile::open("public/pages/index.html").map_err(|n| NotFound(format!("Bad path: {:?}", n)))
+fn index() -> Html<&'static str> {
+
+    lazy_static! {
+        static ref INDEX: String = {
+            construct_page("public/pages/index.html")
+        };
+    }
+
+    Html(&*INDEX)
 }
 
 #[get("/euler")]
